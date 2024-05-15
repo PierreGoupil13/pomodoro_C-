c89bce2a15d0b20a6c12c2e867546aa76858e89b
pierregoupil <p.goupil356@gmail.com>
2024-05-15 10:39:15 +0200

    init

---
+++ BlazorApp1/Components/Pages/Pomodoro.cs
@@ -0,0 +1,6 @@
+namespace BlazorApp1.Components.Pages;
+
+public class Pomodoro
+{
+    
+}
---
+++ BlazorApp1/Components/Pages/Pomodoro.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Components.Pages;
+
+public class Pomodoro : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
